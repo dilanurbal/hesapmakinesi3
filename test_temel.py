eacb16be3834398d834ba79b214f548cdf7e8ec9
aysenciftci23 <aysenciftci29@gmail.com>
2026-07-02 13:19:50 +0300

    düzenlendi.

--- test_temel.py
+++ test_temel.py
@@ -2,7 +2,7 @@ import pytest
 import math
 
 from bilimsel import bilimsel
-# temel.py içinde sınıf olmadığı için doğrudan kullanacağımız fonksiyonu çağırıyoruz:
+
 from temel import hesapla
 
 
@@ -10,8 +10,6 @@ from temel import hesapla
 def bilimsel_motor():
     return bilimsel()
 
-
-# --- BİLİMSEL İŞLEMLER TESTLERİ (Sınıf Yapısı) ---
 def test_sabitler(bilimsel_motor):
     assert bilimsel_motor.pi == math.pi
     assert bilimsel_motor.e == math.e
@@ -34,9 +32,9 @@ def test_bilimsel_hatalar(bilimsel_motor):
     assert "Hata" in str(bilimsel_motor.mod_alma(10, 0))
 
 
-# --- TEMEL İŞLEMLER TESTLERİ (Fonksiyon Yapısı) ---
+
 def test_temel_islemler():
-    # hesapla() fonksiyonuna doğrudan string göndererek test ediyoruz
+
     assert hesapla("5+3") == 8
     assert hesapla("(5+3)×2") == 16
     assert hesapla("√(16)") == 4
@@ -44,6 +42,6 @@ def test_temel_islemler():
 
 
 def test_temel_hatalar():
-    # Güvenlik ve sıfıra bölünme hataları
+
     assert "Hata" in str(hesapla("10÷0"))
     assert "Hata" in str(hesapla("abc"))
